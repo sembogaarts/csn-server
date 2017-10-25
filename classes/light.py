6038562367b402eb753192cae728b9aed9325b63
Sem Bogaarts <sembogaarts@MacBook-Pro-van-Sem.local>
2017-10-25 23:28:03 +0200

    Buzzer added

--- classes/light.py
+++ classes/light.py
@@ -11,13 +11,25 @@ class Light:
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         # Define lights
-        self.buzzer = int(config.get('SENSORS', 'buzzer'))
+        self.green = int(config.get('LIGHTS', 'green'))
+        self.yellow = int(config.get('LIGHTS', 'yellow'))
+        self.red = int(config.get('LIGHTS', 'red'))
         # Setup Lights
-        GPIO.setup(self.buzzer, GPIO.OUT)
-        self.buzz = GPIO.PWM(self.buzzer, 50)
+        GPIO.setup(self.green, GPIO.OUT)
+        GPIO.setup(self.yellow, GPIO.OUT)
+        GPIO.setup(self.red, GPIO.OUT)
 
-    def on(self):
-        self.buzz.start(50)
+    def on(self, light):
+        print("power on for GPIO " + str(light))
+        GPIO.output(light, True)
+        self.state = True
 
-    def off(self):
-        self.buzz.stop()
+    def off(self, light):
+        print("power off for GPIO " + str(light))
+        GPIO.output(light, False)
+        self.state = False
+
+    def clear(self):
+        GPIO.cleanup()
+
+
