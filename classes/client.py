8fd485c47e997f14c7561a96dfad72ca58e0797b
Sem Bogaarts <sembogaarts@MacBook-Pro-van-Sem.local>
2017-10-27 15:42:33 +0200

    Bug

--- classes/client.py
+++ classes/client.py
@@ -57,6 +57,7 @@ class Client:
         for row in clients:
             query = "SELECT status FROM logs WHERE client_id = %s ORDER BY id DESC LIMIT 1"
             status = Database().fetchOne(query, [row['client_id']])
+            print(status)
             if len(status) > 0:
                 row['status'] = status['status']
         return clients
